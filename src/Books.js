adb0a610c5ac2e4dc219469cc0257d6789c8bf99
Danyeal Mateen <danyeal.mateen@hotmail.com>
2023-06-30 12:10:02 +0200

    added irebase

--- src/Books.js
+++ src/Books.js
@@ -1,13 +1,54 @@
 import "./Books.css";
+import { useState } from "react"
 
 const Books = () => {
+
+    const [title, setTitle] = useState(' ');
+    const [pages, setPages] = useState(' ');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch('https://bookapp-135fc-default-rtdb.europe-west1.firebasedatabase.app/books.json', {
+            method: 'POST',
+            body: JSON.stringify({
+                title, pages
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        }).then(data => {
+            setTitle(' ')
+            setPages(' ')
+        }).catch(error => {
+            console.error("Error adding book", error)
+        })
+    }
+
     return (
         <>
             <div className="createNewBookDiv">
-                <button>Create new book</button>
-                <form>
-                    <input type="text" placeholder="Title" />
-                    <input type="text" placeholder="Pages" />
+
+                <form onSubmit={handleSubmit}>
+                    <button type="submit">Create new book</button>
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => {
+                            setTitle(e.target.value)
+                            console.log(e.target.value)
+                        }}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Pages"
+                        value={pages}
+                        onChange={(e) => {
+                            setPages(e.target.value)
+                            console.log(e.target.value)
+                        }}
+                    />
                 </form>
             </div>
         </>
